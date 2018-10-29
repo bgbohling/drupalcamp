a560953351fe06eb95e5b71016dafbb9f8c7b5b8
Dave Reid <dave@davereid.net>
2018-10-29 11:21:24 -0500

    Fixed derivative code, clear cached definitions when guitarist nodes are touched.

--- src/Plugin/Derivative/GuitarDeriver.php
+++ src/Plugin/Derivative/GuitarDeriver.php
@@ -3,37 +3,59 @@
 namespace Drupal\drupalcamp_2018\Plugin\Derivative;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 class GuitarDeriver extends DeriverBase implements ContainerDeriverInterface {
 
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new GuitarDeriver instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
   public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container, $base_plugin_id) {
     return new static(
       $container->get('entity_type.manager')
     );
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getDerivativeDefinitions($base_plugin_definition) {
-    $nodes = $this->entityTypeManager()->getStorage(‘node’)->loadMultiple();
-    $players = $nodes->getQuery()
-      ->condition(‘type’, ‘musician’)
-      ->execute();
     $this->derivatives = [];
-    if (!empty($players)) {
-      foreach ($players as $player) {
-        if ($player->field_instrument->getValue() == 'guitar') {
-          $this->derivatives[$player->id()] = $base_plugin_definition;
-          $this->derivatives[$player->id()][‘name’] = $player->field_name->getValue();
-          $this->derivatives[$player->id()][‘instrument’] = $player->field_axe->getValue();
-        }
+
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    $node_ids = $node_storage->getQuery()
+      ->condition('type', 'musician')
+      ->condition('field_instrument', 'guitar')
+      ->execute();
+
+    if (!empty($node_ids)) {
+      $nodes = $node_storage->loadMultiple($node_ids);
+      foreach ($nodes as $node) {
+        $this->derivatives[$node->id()] = $base_plugin_definition;
+        $this->derivatives[$node->id()][‘name’] = $node->field_name->getValue();
+        $this->derivatives[$node->id()][‘instrument’] = $node->field_axe->getValue();
       }
     }
+
     return $this->derivatives;
   }
 
